0f42d6d0244deef6f278a7f76aafe3f28384573f
davidhcefx <davidhu0903ex3@gmail.com>
2020-11-24 06:17:20 +0800

    test

--- challenge/docker/run.sh
+++ challenge/docker/run.sh
@@ -4,16 +4,13 @@ name="constr"
 ports="80:80"
 
 
-dockerd -H fd:// --containerd=/run/containerd/containerd.sock &
+/app/vendor/docker/docker/dockerd -H fd:// --containerd=/run/containerd/containerd.sock &
 
 # copy binary
 cp ../$name share/$name
 
-# build
-echo "building"
 docker build . -t $name
 
-# run
 docker run -d --name $name \
     --volume $PWD/share/:/home/$name:ro \
     --volume $PWD/xinetd:/etc/xinetd.d/$name:ro \
